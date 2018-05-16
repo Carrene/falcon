d99aaaef4fb702a25453e861cd92dee986b0fdad
Mysterious <ehsan.mashhadi@gmail.com>
2018-05-16 10:58:23 +0430

    Creating android studio project

---
+++ app/src/main/java/de/netalic/falcon/MainActivity.java
@@ -0,0 +1,14 @@
+package de.netalic.falcon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
