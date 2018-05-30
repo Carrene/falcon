9c8cebcd2fe802a69db8f11426877adba11ae6f5
miladsi90 <miladsalimi69@yahoo.com>
2018-05-30 18:07:40 +0430

    Create country code picker #8

--- app/src/main/java/de/netalic/falcon/view/RegistrationFragment.java
+++ app/src/main/java/de/netalic/falcon/view/RegistrationFragment.java
@@ -6,6 +6,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.QuickContactBadge;
+import android.widget.Toast;
+
+import com.mukesh.countrypicker.Country;
+import com.mukesh.countrypicker.CountryPicker;
+import com.mukesh.countrypicker.OnCountryPickerListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import de.netalic.falcon.R;
 import de.netalic.falcon.presenter.RegistrationContract;
@@ -15,12 +26,21 @@ import static com.google.common.base.Preconditions.checkNotNull;
 public class RegistrationFragment extends Fragment implements RegistrationContract.View {
 
     private RegistrationContract.Presenter mPresenter;
+    private CountryPicker mCountryPicker;
+    private Button btButton;
+    private EditText etEditText;
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_registration, container, false);
+        etEditText = root.findViewById(R.id.editText2);
+        btButton = root.findViewById(R.id.button3);
+        setCountryPicker();
+        initListenre();
+        modifyCountryName();
         return root;
     }
 
@@ -47,4 +67,50 @@ public class RegistrationFragment extends Fragment implements RegistrationContra
         mPresenter = checkNotNull(presenter);
 
     }
+
+    public void setCountryPicker() {
+        mCountryPicker =
+                new CountryPicker.Builder().with(getContext())
+                        .listener(new OnCountryPickerListener() {
+                            @Override
+                            public void onSelectCountry(Country country) {
+                                //DO something here
+
+                                etEditText.setText(country.getDialCode());
+                            }
+                        })
+                        .build();
+    }
+
+    public void initListenre() {
+
+        btButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mCountryPicker.showDialog(getFragmentManager());
+
+
+            }
+        });
+
+    }
+
+    private void modifyCountryName() {
+
+        List<Country> countries = mCountryPicker.getAllCountries();
+        List<Country> allCountryModified = new ArrayList<>();
+        for (int i = 0; i < countries.size(); i++) {
+            Country country = countries.get(i);
+            if (country.getDialCode().equals("+972")) {
+                continue;
+            }
+            if (country.getDialCode().equals("+970")) {
+                continue;
+            }
+            allCountryModified.add(country);
+        }
+        mCountryPicker.setCountries(allCountryModified);
+    }
+
 }
