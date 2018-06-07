2b26662a1c7a49e6c1da4e6b0aded4383bceeb71
Ehsan Mashhadi <ehsan.mashhadi@gmail.com>
2018-06-07 15:11:12 +0430

    Add powermoito for mocking static class

--- app/src/test/java/de/netalic/falcon/repository/UserRepositoryTest.java
+++ app/src/test/java/de/netalic/falcon/repository/UserRepositoryTest.java
@@ -1,41 +1,62 @@
 package de.netalic.falcon.repository;
 
+import android.provider.Settings;
+
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.io.IOException;
 import java.util.concurrent.CountDownLatch;
 
+import de.netalic.falcon.MyApp;
 import de.netalic.falcon.model.User;
 import de.netalic.falcon.network.ApiClient;
-import de.netalic.falcon.network.ApiInterface;
 import de.netalic.falcon.repository.user.UserRepository;
+import nuesoft.helpdroid.device.DeviceUtil;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
 
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.mock;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({DeviceUtil.class, Settings.Secure.class,MyApp.class})
+@PowerMockIgnore("javax.net.ssl.*")
 
 public class UserRepositoryTest {
 
-    private UserRepository userRepository;
-    private static MockWebServer mockWebServer;
-    private User user;
+    private UserRepository mUserRepository;
+    private static MockWebServer sMockWebServer;
+    private User mUser;
 
     @Before
     public void setUp() {
 
         MockitoAnnotations.initMocks(this);
-        userRepository = UserRepository.getInstance();
-        mockWebServer = new MockWebServer();
-        //TODO: mock api client address to localhost:8585
+
+        PowerMockito.mockStatic(Settings.Secure.class);
+        PowerMockito.mockStatic(MyApp.class);
+
+        MyApp myApp = mock(MyApp.class);
+        Mockito.when(MyApp.getInstance()).thenReturn(myApp);
+        Mockito.when(DeviceUtil.getSecureId(myApp)).thenReturn("123456");
+
+        mUserRepository = UserRepository.getInstance();
+        sMockWebServer = new MockWebServer();
+
         try {
-            mockWebServer.start(8585);
+            sMockWebServer.start(0);
         } catch (IOException e) {
             e.printStackTrace();
+            Assert.fail("Unable to start server");
         }
     }
 
@@ -44,14 +65,14 @@ public class UserRepositoryTest {
 
         System.out.println(ApiClient.getApiAddress());
         CountDownLatch countDownLatch = new CountDownLatch(1);
-        String json = "{\"phone\":\"989122451075\",\"udid\":\"2b6f0cc904d137be2e1730235f5664094b831186\"}";
-        mockWebServer.enqueue(new MockResponse().setBody(json));
-        user = new User("+981234567890");
+        String json = "{\"phone\":\"989122451075\",\"udid\":\"7C4A8D09CA3762AF61E59520943DC26494F8941B\"}";
+        sMockWebServer.enqueue(new MockResponse().setBody(json));
+        mUser = new User("+981234567890");
 
-        userRepository.claim(user).promise().then(result -> {
+        mUserRepository.claim(mUser).promise().then(result -> {
             Assert.assertNotNull(result.getModel());
             Assert.assertEquals(result.getModel().getPhone(), "+981234567890");
-            Assert.assertEquals(result.getModel().getUdid(), "2b6f0cc904d137be2e1730235f5664094b831186");
+            Assert.assertEquals(result.getModel().getUdid(), "7C4A8D09CA3762AF61E59520943DC26494F8941B");
             countDownLatch.countDown();
 
         }).fail(result -> {
@@ -69,10 +90,11 @@ public class UserRepositoryTest {
     public static void shutdown() {
 
         try {
-            if (mockWebServer != null)
-                mockWebServer.shutdown();
+            if (sMockWebServer != null)
+                sMockWebServer.shutdown();
         } catch (IOException e) {
             e.printStackTrace();
+            Assert.fail("Unable to stop server");
         }
     }
 }
