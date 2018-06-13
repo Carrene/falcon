03e14ecc9e5b6866bc57430241a8ccd17b3fbaa4
miladsi90 <miladsalimi69@yahoo.com>
2018-06-13 11:42:47 +0430

    Done Todo , closed #33

--- app/src/main/java/de/netalic/falcon/view/PhoneConfirmationFragment.java
+++ app/src/main/java/de/netalic/falcon/view/PhoneConfirmationFragment.java
@@ -26,8 +26,7 @@ public class PhoneConfirmationFragment extends Fragment implements PhoneConfirma
     private PhoneConfirmationContract.Presenter mPresenter;
     private TextView mTextViewTimer;
     private View mRoot;
-    //TODO: Milad wrong convention!
-    private boolean isRunning = true;
+    private boolean mIsRunning = true;
 
     @Nullable
     @Override
@@ -104,7 +103,7 @@ public class PhoneConfirmationFragment extends Fragment implements PhoneConfirma
             @Override
             public void onClick(View v) {
 
-                if (isRunning == false) {
+                if (mIsRunning == false) {
                     setTimer();
                 }
             }
@@ -113,26 +112,22 @@ public class PhoneConfirmationFragment extends Fragment implements PhoneConfirma
 
     private void setTimer() {
 
-        //TODO: Milad Wrong name convention and unused object
-        CountDownTimer mCountDownTimer = new CountDownTimer(120000, 1000) {
+        CountDownTimer countDownTimer = new CountDownTimer(120000, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
 
-                //TODO: Milad use better name for variables
-                long minute = TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished);
-                long second = TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) -
+                long minuteُTimer = TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished);
+                long secondTimer = TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) -
                         TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished));
 
-                mTextViewTimer.setText(String.format("%02d:%02d ", minute, second));
-                //TODO: Milad what this line do? repetitive setting true always!
-                isRunning = true;
+                mTextViewTimer.setText(String.format("%02d:%02d ", minuteُTimer, secondTimer));
             }
 
             @Override
             public void onFinish() {
 
                 mTextViewTimer.setText("Resend");
-                isRunning = false;
+                mIsRunning = false;
 
             }
         }.start();
