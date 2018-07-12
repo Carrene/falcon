4904da509b856a9957d6c28bb9abf1d063ae1e28
miladsi90 <miladsalimi69@yahoo.com>
2018-07-12 15:01:58 +0430

    Exchange rate get test , closed #90

--- app/src/androidTest/java/de/netalic/falcon/ExchangeRateRepositoryTest.java
+++ app/src/androidTest/java/de/netalic/falcon/ExchangeRateRepositoryTest.java
@@ -9,28 +9,100 @@ import org.junit.runner.RunWith;
 import java.util.concurrent.CountDownLatch;
 
 import de.netalic.falcon.model.Currency;
+import de.netalic.falcon.model.ExchangeRate;
 import de.netalic.falcon.model.UsdCurrency;
 import de.netalic.falcon.repository.exchangeRate.ExchangeRateRepository;
 
 @RunWith(AndroidJUnit4.class)
 public class ExchangeRateRepositoryTest {
 
+
+    private CountDownLatch mCountDownLatch;
+
     @Test
-    public void testGetExchangeRate_200Response() {
+    public void testGetExchangeRate_200response() {
 
-        CountDownLatch countDownLatch = new CountDownLatch(1);
-
+        mCountDownLatch = new CountDownLatch(1);
         Currency currency = new UsdCurrency();
         ExchangeRateRepository.getInstance().get(currency.getCode(), deal -> {
             Assert.assertNull(deal.getThrowable());
             Assert.assertEquals(deal.getResponse().code(), 200);
-            countDownLatch.countDown();
+            mCountDownLatch.countDown();
         });
 
         try {
-            countDownLatch.await();
+            mCountDownLatch.await();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
+
+    @Test
+    public void testGetExchangeRate_709response() {
+
+        mCountDownLatch = new CountDownLatch(1);
+        Currency currency = new Currency("milad");
+        ExchangeRateRepository.getInstance().get(currency.getCode(), deal -> {
+
+            Assert.assertNull(deal.getThrowable());
+            Assert.assertEquals(deal.getResponse().code(), 709);
+            mCountDownLatch.countDown();
+        });
+
+        try {
+            mCountDownLatch.await();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+
+
+        }
+
+    }
+
+//    @Test
+//    public void testGetExchangeRate_721response() {
+//
+//        mCountDownLatch = new CountDownLatch(1);
+//        Currency currency = new Currency("ir");
+//        ExchangeRateRepository.getInstance().get(currency.getCode(), deal -> {
+//
+//            Assert.assertNull(deal.getThrowable());
+//            Assert.assertEquals(deal.getResponse().code(), 721);
+//            Assert.assertNull(deal.getModel().getBuy(), null);
+//            Assert.assertNull(deal.getModel().getSell(), null);
+//
+//        });
+//
+//        try {
+//            mCountDownLatch.await();
+//
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//
+//
+//        }
+//
+//    }
+//Wait for backend
+//    @Test
+//    public void testGetExchangeRate_failure() {
+//
+//        mCountDownLatch = new CountDownLatch(1);
+//        ExchangeRateRepository.getInstance().get("", deal -> {
+//
+//            Assert.assertNotNull(deal.getThrowable());
+//
+//        });
+//
+//        try {
+//            mCountDownLatch.await();
+//
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//
+//
+//        }
+//
+//    }
 }
