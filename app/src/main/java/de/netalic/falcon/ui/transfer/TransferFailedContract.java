9f743d8dd7ef77baff1876994c800d0f49ab1b79
miladsi90 <miladsalimi69@yahoo.com>
2018-08-28 15:41:02 +0430

    Create transfer failed layout , closed #238 , create transfer failed view , closed #239 , create transfer failed presenter , closed #240

---
+++ app/src/main/java/de/netalic/falcon/ui/transfer/TransferFailedContract.java
@@ -0,0 +1,17 @@
+package de.netalic.falcon.ui.transfer;
+
+import de.netalic.falcon.ui.base.BasePresenter;
+import de.netalic.falcon.ui.base.BaseView;
+
+public interface TransferFailedContract {
+
+    interface  View extends BaseView<Presenter>{
+
+
+    }
+
+    interface Presenter extends BasePresenter{
+
+
+    }
+}
