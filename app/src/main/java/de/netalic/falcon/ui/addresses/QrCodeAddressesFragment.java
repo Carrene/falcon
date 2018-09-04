547b0ca9ef7d8734618f348b2270de8022d7f5a8
miladsi90 <miladsalimi69@yahoo.com>
2018-09-04 17:32:56 +0430

    Implement menu , share button and download button in QR code addresses , closed #267

--- app/src/main/java/de/netalic/falcon/ui/addresses/QrCodeAddressesFragment.java
+++ app/src/main/java/de/netalic/falcon/ui/addresses/QrCodeAddressesFragment.java
@@ -1,17 +1,29 @@
 package de.netalic.falcon.ui.addresses;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.File;
+
 import de.netalic.falcon.R;
+import de.netalic.falcon.util.ScreenshotUtil;
+import de.netalic.falcon.util.SnackbarUtil;
+
+import static com.google.common.base.Preconditions.checkNotNull;
 
 public class QrCodeAddressesFragment extends Fragment implements QrCodeAddressesContract.View {
 
@@ -22,6 +34,11 @@ public class QrCodeAddressesFragment extends Fragment implements QrCodeAddresses
     private Bitmap mBitmapQrCode;
     private View mRoot;
     private String mCurrencyCode;
+    private static final int REQUEST_PERMISSIONS = 1;
+    private static final String ALPHA_PATH = "/Alpha";
+    private static final String CHARGE_PATH = "/Addresses";
+    private static final int IMAGE_QUALITY = 100;
+    private View mScreenshotView;
 
 
     @Nullable
@@ -31,6 +48,7 @@ public class QrCodeAddressesFragment extends Fragment implements QrCodeAddresses
         mRoot=inflater.inflate(R.layout.fragment_qrcodeaddresses,null);
         mBitmapQrCode = getArguments().getParcelable("qr");
         mCurrencyCode=getArguments().getString("currencyCode");
+        setHasOptionsMenu(true);
         return mRoot;
     }
 
@@ -73,6 +91,7 @@ public class QrCodeAddressesFragment extends Fragment implements QrCodeAddresses
 
         mTextViewWalletType=mRoot.findViewById(R.id.textview_qrcodeaddresses_wallettype);
         mImageViewQrCode=mRoot.findViewById(R.id.imageview_qrcodeaddresses_qr);
+        mScreenshotView = mRoot.findViewById(R.id.imageview_qrcodeaddresses_qr);
     }
 
     public void setImageQr() {
@@ -83,6 +102,77 @@ public class QrCodeAddressesFragment extends Fragment implements QrCodeAddresses
     private void setWalletAddress(){
 
         mTextViewWalletType.setText(mCurrencyCode+" "+"wallet");
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_everywhere_sharedownloadtoolbar, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.item_everywhere_download: {
+
+                requestPermissionSave();
+                break;
+            }
+
+            case R.id.item_everywhere_share: {
+
+                requestPermissionShare();
+                break;
+            }
+
+        }
+        return true;
+    }
+
+    private void requestPermissionShare() {
+
+
+        int checkPermission = ContextCompat.checkSelfPermission(checkNotNull(getContext()), Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (checkPermission != PackageManager.PERMISSION_GRANTED) {
+
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_PERMISSIONS);
+        } else {
+
+            File file = ScreenshotUtil.saveScreenshot(ScreenshotUtil.takeScreenshot(mScreenshotView), IMAGE_QUALITY, ALPHA_PATH, CHARGE_PATH);
+            ScreenshotUtil.shareScreenshot(file, getContext());
+
+        }
+    }
+
+    private void requestPermissionSave() {
+
+
+        int checkPermission = ContextCompat.checkSelfPermission(checkNotNull(getContext()), Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (checkPermission != PackageManager.PERMISSION_GRANTED) {
+
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_PERMISSIONS);
+        } else {
+
+            ScreenshotUtil.saveScreenshot(ScreenshotUtil.takeScreenshot(mScreenshotView), IMAGE_QUALITY, ALPHA_PATH, CHARGE_PATH);
+            SnackbarUtil.showSnackbar(mRoot, getContext().getString(R.string.qrcodecompleted_imagesaved), getContext());
+
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_PERMISSIONS && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+
+            SnackbarUtil.showSnackbar(mRoot, getContext().getString(R.string.everywhere_permissionallowed), getContext());
+        } else {
+
+            SnackbarUtil.showSnackbar(mRoot, getContext().getString(R.string.everywhere_permissiondenied), getContext());
+
+        }
     }
 
 }
