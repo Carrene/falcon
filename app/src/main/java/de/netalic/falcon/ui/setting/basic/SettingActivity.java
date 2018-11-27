8e363a2782bf5915e1460367735b42faebb3fe31
hyhashemi <hanieh.y.hashemi@gmail.com>
2018-11-27 19:01:08 +0330

    Changed image view of settings, but the  collapsing textview not working

--- app/src/main/java/de/netalic/falcon/ui/setting/basic/SettingActivity.java
+++ app/src/main/java/de/netalic/falcon/ui/setting/basic/SettingActivity.java
@@ -6,10 +6,17 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.content.ContextCompat;
 
+import de.netalic.falcon.MyApp;
 import de.netalic.falcon.R;
+import de.netalic.falcon.data.model.User;
+import de.netalic.falcon.data.repository.base.RepositoryLocator;
+import de.netalic.falcon.data.repository.user.UserRealmRepository;
+import de.netalic.falcon.data.repository.user.UserRepository;
 import de.netalic.falcon.ui.base.BaseActivity;
 import de.netalic.falcon.ui.dashboard.DashboardActivity;
 import de.netalic.falcon.util.ActivityUtil;
+import nuesoft.helpdroid.network.SharedPreferencesJwtPersistor;
+import nuesoft.helpdroid.util.Parser;
 
 public class SettingActivity extends BaseActivity {
 
@@ -55,15 +62,22 @@ public class SettingActivity extends BaseActivity {
     }
 
     private void setName() {
+        SharedPreferencesJwtPersistor sharedPreferencesJwtPersistor = new SharedPreferencesJwtPersistor(MyApp.getInstance());
+        String token = sharedPreferencesJwtPersistor.get();
+        int id = (int) Parser.getTokenBody(token).get("id");
+        RepositoryLocator.getInstance().getRepository(UserRepository.class).get(id, deal -> {
+            if (deal.getThrowable() == null) {
+                mCollapsingToolbarLayout.setTitle(deal.getModel().getPhone());
+            }
+        });
 
-        mCollapsingToolbarLayout.setTitle("Ehsan");
     }
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
 
-        Intent intent=new Intent(this,DashboardActivity.class);
+        Intent intent = new Intent(this, DashboardActivity.class);
         startActivity(intent);
     }
 
